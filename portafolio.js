7e471f8097bec031357f84b25494fbefeb14260c
favicuna <34049107+favicuna@users.noreply.github.com>
2019-08-31 12:47:26 -0400

    Add files via upload

--- portafolio.js
+++ portafolio.js
@@ -1,43 +1,100 @@
-var trabajos = [{
-    figure: "media/trabajo-01-desfile.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de primer trabajo",
-    about: "Texto simulado de descripción de primer trabajo"
-}, {
-    figure: "media/trabajo-02-app.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de segundo trabajo",
-    about: "Texto simulado de descripción de segundo trabajo"
-}, {
-    figure: "media/trabajo-03-homy.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de tercer trabajo",
-    about: "Texto simulado de descripción de tercer trabajo"
-}, {
-    figure: "media/trabajo-04-editorial.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de cuarto trabajo",
-    about: "Texto simulado de descripción de cuarto trabajo"
-}, {
-    figure: "media/trabajo-05-artesania.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de quinto trabajo",
-    about: "Texto simulado de descripción de quinto trabajo"
-}, {
-    figure: "media/trabajo-06-textil.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de sexto trabajo",
-    about: "Texto simulado de descripción de sexto trabajo"
-}, {
-    figure: "media/trabajo-07-materiales.jpg",
+ var trabajos = [{
+     figure: "Media/Benja1.jpg",
+     alt: "Descripción de la imagen",
+     title: "Título de pjAJAjajAajjAJAAJrimer trabajo",
+     about: "Texto simulado de descripción de primer trabajo",
+     color_letra: "white",
+     color_fondo: "#8100ea",
+     
+     
+
+ }, {
+     figure: "Media/Vicu4.jpg",
+     alt: "Descripción de la imagen",
+     title: "Título de segundo trabajo",
+     about: "Texto simulado de descripción de segundo trabajo",
+     color_letra: "white",
+     color_fondo: "#2d23ea",
+     
+ }, {
+     figure: "Media/Paula1.jpg",
+     alt: "Descripción de la imagen",
+     title: "Título de tercer trabajo",
+     about: "Texto simulado de descripción de tercer trabajo",
+     color_letra: "Black",
+     color_fondo: "#f49595",
+ }, {
+     figure: "Media/Vicu2.jpg",
+     alt: "Descripción de la imagen",
+     title: "Título de cuarto trabajo",
+     about: "Texto simulado de descripción de cuarto trabajo",
+     color_letra: "white",
+     color_fondo: "#2d23ea",
+ }, {
+     figure: "Media/Benja2.jpg",
+     alt: "Descripción de la imagen",
+     title: "Título de quinto trabajo",
+     about: "Texto simulado de descripción de quinto trabajo",
+     color_letra: "white",
+     color_fondo: "#8100ea",
+ }, {
+     figure: "Media/Vicu1.jpg",
+     alt: "Descripción de la imagen",
+     title: "PEPS: Prepara, Emplata, Porciona",
+     about: "Sistema lúdico de organización para la comida de los niños marca ILKO, con el fin de promover una alimentación porcionada y la participación de los mismos en la preparación.",
+     color_letra: "white",
+     color_fondo: "#2d23ea",
+ }, {
+     figure: "Media/Paula2.jpg",
+     alt: "Descripción de la imagen",
+     title: "Título de séptimo trabajo",
+     about: "Texto simulado de descripción de séptimo trabajo",
+     color_letra: "Black",
+     color_fondo: "#f49595",
+     
+ },
+ {
+    figure: "Media/Paula3.jpg",
     alt: "Descripción de la imagen",
     title: "Título de séptimo trabajo",
-    about: "Texto simulado de descripción de séptimo trabajo"
-}];
-// LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
-$.each(trabajos, function(i, val) {
-    var uno = '<figure><img src="' + trabajos[i].figure + '" alt="' + trabajos[i].alt + '"/></figure>';
-    var dos = '<h4>' + trabajos[i].title + '</h4>';
-    var tres = '<p>' + trabajos[i].about + '</p>';
-    $('#portafolio').append('<div class="item">' + uno + dos + tres + '</div>')
-});
+    about: "Texto simulado de descripción de séptimo trabajo",
+    color_letra: "Black",
+     color_fondo: "#f49595",
+},
+{
+    figure: "Media/Benja3.jpg",
+    alt: "Descripción de la imagen",
+    title: "Título de séptimo trabajo",
+    about: "Texto simulado de descripción de séptimo trabajo",
+    color_letra: "white",
+     color_fondo: "#8100ea",
+},{
+    figure: "Media/Benja4.jpg",
+    alt: "Descripción de la imagen",
+    title: "Título de séptimo trabajo",
+    about: "Texto simulado de descripción de séptimo trabajo",
+    color_letra: "white",
+     color_fondo: "#8100ea",
+}
+,{
+    figure: "Media/Paula4.jpg",
+    alt: "Descripción de la imagen",
+    title: "Título de séptimo trabajo",
+    about: "Texto simulado de descripción de séptimo trabajo",
+    color_letra: "Black",
+     color_fondo: "#f49595",
+    
+}
+
+];
+//   LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
+ $.each(trabajos, function(i, val) {
+     var uno = '<figure><img src=' + trabajos[i].figure + ' alt=' + trabajos[i].alt + '/></figure>';
+     var dos = '<h4><font color= ' + trabajos[i].color_letra+'>' +  trabajos[i].title + '</font></h4>';
+     var tres = '<p><font color= ' + trabajos[i].color_letra+'>' + trabajos[i].about + '</font></p>';
+     $('#portafolio').append('<div class="item" style = "background-color: '+trabajos[i].color_fondo+'" >' + uno + dos + tres + '</div>')
+ });
+
+ 
+
+
